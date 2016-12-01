513ccce0d2682cd0e649ac171fd76dea962a431d
hjvanniekerk <hjvanniekerk@gmail.com>
2016-12-01 11:15:12 +0200

    newpage

---
+++ ISO55K/ISO55K/Controllers/ISO55KController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISO55K.Controllers
+{
+    public class ISO55KController : Controller
+    {
+        // GET: ISO55K
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
